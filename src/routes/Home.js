4f152b3a4e9bbdb81b11dbc670541f2c346d842d
Rohit Pandey <rd8614196@gmail.com>
2024-12-06 18:55:06 +0530

    Footer Added

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,13 +4,14 @@ import { gridContainer } from '../utils/exportStyles';
 import { home } from '../utils/exportContent';
 
 const Home = () => {
-
-    useLayoutEffect(() => { document.title="Data Structure Visualizer" } , [])
+    useLayoutEffect(() => { document.title = "Data Structure Visualizer" }, []);
 
     return (
-        <div className="home-page">
-            <div style={{display:"flex", width:"100%", justifyContent:"center"}}>
-            <h3 style={{ fontSize: "2vw", textTransform: 'uppercase' , color : "white"}}>Data Structure and Algorithms Visualizer</h3>
+        <>       <div className="home-page">
+            <div style={{ display: "flex", width: "100%", justifyContent: "center" }}>
+                <h3 style={{ fontSize: "2vw", textTransform: 'uppercase', color: "white" }}>
+                    Data Structure and Algorithms Visualizer
+                </h3>
             </div>
             <div style={gridContainer}>
                 {
@@ -25,7 +26,18 @@ const Home = () => {
                 }
             </div>
         </div>
-    )
+        <footer style={{ 
+                backgroundColor: "#333", 
+                color: "white", 
+                textAlign: "center", 
+                padding: "20px", 
+                marginTop: "30px" 
+            }}>
+                <p>&copy; {new Date().getFullYear()} Data Structure Visualizer. All rights reserved.</p>
+                <p>Designed and developed with 💻 by <a href="https://github.com/rohitPandey469" style={{ color: "#00d8ff" }}>Rohit Pandey</a></p>
+            </footer>
+        </>
+    );
 };
 
 export default Home;
